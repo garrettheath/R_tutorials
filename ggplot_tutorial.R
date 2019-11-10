271f0c6874df17a9bec9b6a208564601b35ceb5f
Garrett Heath <garrettheath@Garretts-iMac.fios-router.home>
2019-11-10 09:32:54 -0500

    This change is fine

--- ggplot_tutorial.R
+++ ggplot_tutorial.R
@@ -1,12 +1,14 @@
 #set working directory
-setwd("~/Documents/R/R_Data/tutorial")
+setwd("~/Documents/R/R_Data/R_tutorials")
 
+install.packages("ggplot2")
+install.packages("tidyverse")
 library(ggplot2)
 
 #import the housing data
 data <- read.csv("./data/housing.csv")
 
 head(data)
-ggplot(data, aes(housing_median_age, total_homes) + geom_point())
+ggplot(data, aes(housing_median_age, total_rooms))+ geom_point()
 
        
